626da9308844791889fe113a148b04a0eaa97606
Achim Rolle <achimrolle@googlemail.com>
2020-09-06 18:30:42 +0200

    feat: add wip message bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { Box } from "@chakra-ui/core";
 
 import type { Skill } from "./api/skills";
 import { Header } from "../components/Header";
@@ -34,6 +35,18 @@ const HomePage: NextPage<Props> = ({ skills }) => {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"
         />
       </Head>
+      <Box
+        zIndex={10}
+        position={"fixed"}
+        bottom={0}
+        w={"full"}
+        p={4}
+        textAlign={"center"}
+        bg={"green.100"}
+        color={"green.800"}
+      >
+        This page is a work in progress
+      </Box>
       <Header />
       <HeroSection />
       <SkillsSection skills={skills} />
