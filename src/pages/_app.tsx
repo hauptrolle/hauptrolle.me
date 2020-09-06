354c0a2577af114ccf44c0849d59c6fe665d3983
Achim Rolle <achimrolle@googlemail.com>
2020-09-06 16:36:23 +0200

    feat: setup custom theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,25 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/core";
+import { ChakraProvider, theme } from "@chakra-ui/core";
+import { merge } from "@chakra-ui/utils";
+import Head from "next/head";
+
+const hauptrolleTheme = merge(theme, {
+  fonts: {
+    body: "'Inter', sans-serif",
+    heading: "'Inter', sans-serif",
+    mono: "Menlo, monospace",
+  },
+});
 
 function HauptrolleApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS={true}>
+    <ChakraProvider theme={hauptrolleTheme} resetCSS={true}>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
