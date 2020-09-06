09908a0c8307ab87c91e22fdb89e6e1f352a72a7
Achim Rolle <achimrolle@googlemail.com>
2020-09-06 19:32:15 +0200

    refactor: use local hosted fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import { ChakraProvider, theme } from "@chakra-ui/core";
 import { merge } from "@chakra-ui/utils";
 import Head from "next/head";
 
+import "../css/fonts.css";
+import React from "react";
+
 const hauptrolleTheme = merge(theme, {
   styles: {
     global: {
@@ -22,9 +25,16 @@ function HauptrolleApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={hauptrolleTheme} resetCSS={true}>
       <Head>
+        <title>Achim Rolle - Frontend developer & web designer</title>
+        <meta
+          name={"description"}
+          content={
+            "I am Achim. A self taught frontend developer & web designer from Hessen/Germany"
+          }
+        />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
-          rel="stylesheet"
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"
         />
       </Head>
       <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Head from "next/head";
 import { Box } from "@chakra-ui/core";
 
 import { Header } from "../components/Header";
@@ -11,19 +10,6 @@ import { Footer } from "../components/Footer";
 const HomePage = () => {
   return (
     <>
-      <Head>
-        <title>Achim Rolle - Frontend developer & web designer</title>
-        <meta
-          name={"description"}
-          content={
-            "I am Achim. A self taught frontend developer & web designer from Hessen/Germany"
-          }
-        />
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"
-        />
-      </Head>
       <Box
         zIndex={10}
         position={"fixed"}
