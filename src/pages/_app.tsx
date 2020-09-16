cff66cbaa652526ae8ff601f2c81d52f2ea9cf48
Achim Rolle <achimrolle@googlemail.com>
2020-09-16 20:13:48 +0200

    feature: add splitbee tracking code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -47,6 +47,7 @@ function HauptrolleApp({ Component, pageProps }: AppProps) {
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨‍💻</text></svg>"
         />
+        <script async src="https://cdn.splitbee.io/sb.js" />
       </Head>
       <Component {...pageProps} />
     </ChakraProvider>
