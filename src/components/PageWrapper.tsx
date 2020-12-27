4218b39845b5afa11ca5e2384c790e0115371b30
Achim Rolle <achimrolle@googlemail.com>
2020-12-27 13:13:37 +0100

    fix: update container maxWidth after dependency updates

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -2,5 +2,5 @@ import { FC } from "react";
 import { Container } from "@chakra-ui/react";
 
 export const PageWrapper: FC = ({ children }) => {
-  return <Container maxW={"lg"}>{children}</Container>;
+  return <Container maxW={"5xl"}>{children}</Container>;
 };
